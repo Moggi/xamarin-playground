19d12147fe5e0b1da19e6fa22a9a6ad4cfd743d3
Paulo Moggi <paulohpmoggi@gmail.com>
2016-01-20 16:37:13 -0200

    Restaura os packages para a versao que funcione
    - Atualiza pacotes do projeto
    - Atualiza imagem do aplicativo e da LoginPage

---
+++ gca-mobile/View/MyPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace gcamobile
+{
+	public partial class MyPage : ContentPage
+	{
+		public MyPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
